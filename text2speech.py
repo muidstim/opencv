1b6f8b811690d9723300f613dfa0e9167ec6e675
muidstim <tim.gir@mahidol.edu>
2018-11-23 10:54:28 +0700

    Update and rename voicetest1.py to text2speech.py

--- text2speech.py
+++ text2speech.py
@@ -1,10 +1,12 @@
+# CLI install instructions:
 # sudo pip install pyttsx3
 
 import pyttsx3
+
 engine = pyttsx3.init()
 engine.say("Hello this is me talking")
-engine.setProperty('rate',100)  #120 words per minute
-engine.setProperty('volume',0.9)
+engine.setProperty('rate', 100)  #100 words per minute
+engine.setProperty('volume', 0.9)
 
 
 engine.runAndWait()
